7f80a36c6426bfcc8748b3e103cc052df06f65fc
MossGoblin <hugolabs@gmail.com>
2018-03-08 19:34:05 +0200

    Started the IO part - writing in the log file and committing the Seed array to a file

---
+++ 00 MAIN/01 PARSER Test Module/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _01_PARSER_Test_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logMessage = "";
+            // 0. Size of the project array - 32*32
+            // 1. Seed a random array
+            // 2. Write the seed into SeedMap.txt
+
+            // 1. Seed a random array
+            Random buzzer = new Random();
+            int[] seedArray = new int[1024];
+
+            for (int ii = 0; ii < seedArray.Length-1; ii++)
+            {
+                int newRND = buzzer.Next(0, 256);
+                seedArray[ii] = newRND;
+            }
+
+            // Write the array into SeedMap.txt
+            string seedArrayString = string.Join("|" , seedArray);
+            System.IO.File.WriteAllText(@"..\data\SeedMap.txt", seedArrayString);
+
+            // Note Down in WorkLog.txt
+
+            logMessage = $"[{DateTime.Now}] ReSeed {Environment.NewLine}";
+            System.IO.File.AppendAllText(@"..\data\WorkLog.txt", logMessage);
+            logMessage = $"[{DateTime.Now}] Seed -> SeedMap.txt {Environment.NewLine}";
+            System.IO.File.AppendAllText(@"..\data\WorkLog.txt", logMessage);
+
+            Console.WriteLine();
+        }
+    }
+}
